f6705cc364043d3691be36aebfec1b5be678fa64
Sergeev_DY <folcon2001@gmail.com>
2026-03-25 15:27:46 +0300

    запросы и ответы сервера

--- httpserver/cmd/main.go
+++ httpserver/cmd/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"main/configs"
+	"main/internal/auth"
 	"net/http"
 )
 
 func main() {
-
+	conf := configs.LoadConfig()
 	router := http.NewServeMux()
-	NewHelloHandler(router)
+	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
+		Config: conf,
+	})
 
 	server := http.Server{
 		Addr:    ":8081",
---
+++ httpserver/internal/auth/handler.go
@@ -0,0 +1,43 @@
+package auth
+
+import (
+	"fmt"
+	"main/configs"
+	"main/pkg/res"
+	"net/http"
+)
+
+type AuthHandlerDeps struct {
+	*configs.Config
+}
+
+type AuthHandler struct {
+	*configs.Config
+}
+
+func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
+	handler := &AuthHandler{
+		Config: deps.Config,
+	}
+	router.HandleFunc("POST /auth/login", handler.Login())
+	router.HandleFunc("POST /auth/register", handler.Register())
+}
+
+func (handler *AuthHandler) Login() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		fmt.Println(handler.Config.Auth.Secret)
+		fmt.Println("Login")
+		data := LoginResonse{
+			Token: "123",
+		}
+		res.Json(w, data, 200)
+
+	}
+}
+
+func (handler *AuthHandler) Register() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println("Register")
+	}
+}
